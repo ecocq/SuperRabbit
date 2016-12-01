4b6155a39d95351241e7d83470224e847a8f33d8
Loïc Leinot <cioltoniel@hotmail.fr>
2016-12-01 18:16:37 -0500

    readapt matrix

--- ProjetGl/transformations_mat.cpp
+++ ProjetGl/transformations_mat.cpp
@@ -9,7 +9,7 @@
 
 #define PI 3.14159265
 
-glm::vec4 translation(glm::vec3 trans, glm::vec4 pos) {
+glm::mat4 translation(glm::vec3 trans) {
 	float trans_[16] = {
 		1, 0, 0, trans.x,
 		0, 1, 0, trans.y,
@@ -17,11 +17,11 @@ glm::vec4 translation(glm::vec3 trans, glm::vec4 pos) {
 		0, 0, 0, 1
 	};
 	glm::mat4 trans_mat = glm::make_mat4(trans_);
-	return pos * trans_mat;
+	return trans_mat;
 }
 
-glm::vec4 rotation_x(float angle_d, glm::vec4 pos) {
-	float angle = angle_d * PI / 180.0;
+glm::mat4 rotation_x(float angle_d) {
+	float angle = angle_d * PI / 180.0f;
 	float rot_[16] = {
 		1, 0, 0, 0,
 		0, cos(angle), sin(angle), 0,
@@ -29,10 +29,10 @@ glm::vec4 rotation_x(float angle_d, glm::vec4 pos) {
 		0, 0, 0, 1
 	};
 	glm::mat4 rot_mat = glm::make_mat4(rot_);
-	return pos * rot_mat;
+	return rot_mat;
 }
 
-glm::vec4 rotation_y(float angle_d, glm::vec4 pos) {
+glm::mat4 rotation_y(float angle_d) {
 	float angle = angle_d * PI / 180.0;
 	float rot_[16] = {
 		cos(angle), 0, -sin(angle), 0,
@@ -41,10 +41,10 @@ glm::vec4 rotation_y(float angle_d, glm::vec4 pos) {
 		0, 0, 0, 1
 	};
 	glm::mat4 rot_mat = glm::make_mat4(rot_);
-	return pos * rot_mat;
+	return rot_mat;
 }
 
-glm::vec4 rotation_z(float angle_d, glm::vec4 pos) {
+glm::mat4 rotation_z(float angle_d) {
 	float angle = angle_d * PI / 180.0;
 	float rot_[16] = {
 		cos(angle), -sin(angle), 0, 0,
@@ -53,5 +53,5 @@ glm::vec4 rotation_z(float angle_d, glm::vec4 pos) {
 		0, 0, 0, 1
 	};
 	glm::mat4 rot_mat = glm::make_mat4(rot_);
-	return pos * rot_mat;
+	return rot_mat;
 }
