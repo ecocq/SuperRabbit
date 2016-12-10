390ee798eea197b979f9606858d162c2b6983ac9
Loïc Leinot <cioltoniel@hotmail.fr>
2016-12-10 12:56:55 -0500

    mouse controls (need to change behaviour for some angles)

--- ProjetGl/Camera.cpp
+++ ProjetGl/Camera.cpp
@@ -17,31 +17,30 @@ Camera::Camera()
 
 void Camera::execute(GLFWwindow *window)
 {
-	glm::vec3 cartesian_dir =  rotation_y(angle) * glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
-	glm::vec3 cartesian_dir_right =  rotation_y(angle) * glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);
+	double xpos, ypos;
+	glfwGetCursorPos(window, &xpos, &ypos);
+
+	glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+	horizontalAngle -= mouseSpeed * float(1024 / 2 - xpos);
+	verticalAngle += mouseSpeed * float(768 / 2 - ypos);
+
+	glm::vec3 cartesian_dir = rotation_z(verticalAngle) * rotation_y(horizontalAngle) * glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::vec3 cartesian_dir_right = rotation_z(verticalAngle) * rotation_y(horizontalAngle) * glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);
 
 	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-		position = translation(cartesian_dir * speed) * position;
+		position = orthographic_projection(glm::vec3(0,1, 0)) * translation(cartesian_dir * speed) * position;
 	}
 
 	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-		position = translation(-cartesian_dir * speed) * position;
+		position = orthographic_projection(glm::vec3(0, 1, 0)) * translation(-cartesian_dir * speed) * position;
 	}
 
 	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-		position = translation(cartesian_dir_right * speed) * position;
+		position = orthographic_projection(glm::vec3(0, 1, 0)) * translation(cartesian_dir_right * speed) * position;
 	}
 
 	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-		position = translation(-cartesian_dir_right * speed) * position;
-	}
-
-	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
-		angle += 1.0f;
-	}
-
-	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
-		angle -= 1.0f;
+		position = orthographic_projection(glm::vec3(0, 1, 0)) *translation(-cartesian_dir_right * speed) * position;
 	}
 
 	CamViewMatrice = glm::lookAt(glm::vec3(position), glm::vec3(position) + cartesian_dir, glm::vec3(0,1,0));
