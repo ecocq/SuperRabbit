5078f0a84ab90230613f7a9e8abd69904b8a8244
Loïc Leinot <cioltoniel@hotmail.fr>
2016-12-13 13:10:38 -0500

    fix rabbit not moving

--- ProjetGl/main.cpp
+++ ProjetGl/main.cpp
@@ -108,7 +108,7 @@ int main(void)
 
 	//Init objects
 	std::vector<PhysicalObject*> objects;
-	MovableObject* m_rabbit = new MovableObject("obj/Rabbit.obj", glm::vec3(1.0f, 1.0f, 1.0f), fragColor, window, programID, glm::vec3(2, -3, 0));
+	MovableObject* m_rabbit = new MovableObject("obj/Rabbit.obj", glm::vec3(1.0f, 1.0f, 1.0f), fragColor, window, programID, glm::vec3(2, -2.9, 0));
 	PhysicalObject* carrot = new PhysicalObject("obj/carrot.obj", glm::vec3(1.0f, 0.6f, 0.03f), fragColor, window, programID, translation(glm::vec3(1.5, -2, 1.5)) * rotation_x(90));
 	objects.push_back(m_rabbit);
 	objects.push_back(carrot);
