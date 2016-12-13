6cbae5d9e8f3dcfa7442eeb67f5f1cbde8017734
Elodie <Elodie@DESKTOP-EH72LBT>
2016-12-12 22:35:16 -0500

    Change control sensibilité 0.2 -> 0.1

--- ProjetGl/MovableObject.cpp
+++ ProjetGl/MovableObject.cpp
@@ -51,29 +51,29 @@ void MovableObject::setObjects(std::vector<PhysicalObject*> _objects) {
 
 void MovableObject::applyTransformsFromControls() {
 	if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) {
-		applyTranslation(glm::vec3(0, 0, 0.2));
+		applyTranslation(glm::vec3(0, 0, 0.1));
 	}else if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) {
-		applyTranslation(glm::vec3(0, 0, -0.2));
+		applyTranslation(glm::vec3(0, 0, -0.1));
 	}else if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) {
-		applyTranslation(glm::vec3(-0.2, 0, 0));
+		applyTranslation(glm::vec3(-0.1, 0, 0));
 	}else if(glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) {
-		applyTranslation(glm::vec3(0.2 ,0, 0));
+		applyTranslation(glm::vec3(0.1 ,0, 0));
 	}else if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
-		applyTranslation(glm::vec3(0, 0.2, 0));
+		applyTranslation(glm::vec3(0, 0.1, 0));
 	}else if (glfwGetKey(window, GLFW_KEY_SEMICOLON) == GLFW_PRESS) {
-		applyTranslation(glm::vec3(0, -0.2, 0));
+		applyTranslation(glm::vec3(0, -0.1, 0));
 	}else if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) {
-		applyRotation(0, 2, 0);
+		applyRotation(0, 0.1, 0);
 	}else if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS) {
-		applyRotation(0, -2, 0);
+		applyRotation(0, -0.1, 0);
 	}else if (glfwGetKey(window, GLFW_KEY_Y) == GLFW_PRESS) {
-		applyRotation(0, 0, -2);
+		applyRotation(0, 0, -0.1);
 	}else if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS) {
-		applyRotation(0, 0, 2);
+		applyRotation(0, 0, 0.1);
 	}else if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS) {
-		applyRotation(2, 0, 0);
+		applyRotation(0.1, 0, 0);
 	}else if (glfwGetKey(window, GLFW_KEY_COMMA) == GLFW_PRESS) {
-		applyRotation(-2, 0, 0);
+		applyRotation(-0.1, 0, 0);
 	}
 
 	if (wheel != 0) {
@@ -92,11 +92,11 @@ void MovableObject::applyTransformsFromControls() {
 	//applyRotationAroundAxis
 
 	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
-		applyScale(glm::vec3(1.2, 1.2, 1.2));
+		applyScale(glm::vec3(1.1, 1.1, 1.1));
 	}
 
 	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
-		applyScale(glm::vec3(0.8, 0.8, 0.8));
+		applyScale(glm::vec3(0.9, 0.9, 0.9));
 	}
 
 	if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
